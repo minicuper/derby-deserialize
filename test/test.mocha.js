3a0ff04c6cdb56f037b63e55fab420863112df3b
Artur Zayats <zag2art@gmail.com>
2014-08-05 14:43:26 +0400

    refactoring - add view param to deserialize funct

--- test/test.mocha.js
+++ test/test.mocha.js
@@ -13,21 +13,21 @@ describe('Tags', function() {
 
   it('input', function() {
     var html = '<input/>';
-    expect(getTemplate('tmp', html).deserialize()).equal(html);
+    expect(getView('tmp', html).deserialize()).equal(html);
   });
 
   it('<div><input/></div>', function() {
     var html = '<div><input/></div>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{test}}', function() {
     var html = '{{test}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 });
 
@@ -36,35 +36,35 @@ describe('Attributes', function() {
 
   it('input class="test"', function() {
     var html = '<input class="test"/>';
-    expect(getTemplate('tmp', html).deserialize()).equal(html);
+    expect(getView('tmp', html).deserialize()).equal(html);
   });
 
   it('input as="test"', function() {
     var html = '<input as="test"/>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('input on-click="hello()"', function() {
     var html = '<input on-click="hello()"/>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('input on-click="hello(a,b,c)"', function() {
     var html = '<input on-click="hello(a,b,c)"/>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('input class="{{test}}"', function() {
     var html = '<input class="{{test}}"/>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
 });
@@ -74,44 +74,44 @@ describe('Expressions', function() {
 
   it('{{a+b*10000}}', function() {
     var html = '{{a+b*10000}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{if a+b*10000}}{{/}}', function() {
     var html = '{{if a+b*10000}}{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('<input class="{{a+b*10000}}"/>', function() {
     var html = '<input class="{{a+b*10000}}"/>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{a.b}}', function() {
     var html = '{{a.b}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{a.b[c]}}', function() {
     var html = '{{a.b[c]}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{@content}}', function() {
     var html = '{{@content}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
 });
@@ -121,30 +121,30 @@ describe('bound/unbound', function() {
 
   it('{{bound a}}', function() {
     var html = '{{bound a}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{unbound a}}', function() {
     var html = '{{unbound a}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{unbound}}<br/>{{/}}', function() {
     var html = '{{unbound}}<br/>{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{bound}}<br/>{{/}}', function() {
     var html = '{{bound}}<br/>{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
 });
@@ -154,44 +154,44 @@ describe('if else (unless)', function() {
 
   it('{{if a}}{{/}}', function() {
     var html = '{{if a}}{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{if a}}<input/>{{/}}', function() {
     var html = '{{if a}}<input/>{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{if a}}<input/>{{else}}hello{{/}}', function() {
     var html = '{{if a}}<input/>{{else}}hello{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{if a}}<input/><br/>{{else}}hello{{/}}', function() {
     var html = '{{if a}}<input/><br/>{{else}}hello{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{if a}}<br/>{{else if b}}hello{{/}}', function() {
     var html = '{{if a}}<br/>{{else if b}}hello{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{unless a}}<br/>{{else if b}}hello{{/}}', function() {
     var html = '{{unless a}}<br/>{{else if b}}hello{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
 });
@@ -201,24 +201,24 @@ describe('on / with', function() {
 
   it('{{with a}}<br/>{{/}}', function() {
     var html = '{{with a}}<br/>{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{with a as #b}}<br/>{{/}}', function() {
     var html = '{{with a as #b}}<br/>{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
 
   it('{{on a}}<br/>{{/}}', function() {
     var html = '{{on a}}<br/>{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
 
@@ -229,23 +229,23 @@ describe('each', function() {
 
   it('{{each abs}}<br/>{{/}}', function() {
     var html = '{{each abs}}<br/>{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{each abs as #a}}<br/>{{/}}', function() {
     var html = '{{each abs as #a}}<br/>{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('{{each abs as #a}}<br/>{{else}}<input/>{{/}}', function() {
     var html = '{{each abs as #a}}<br/>{{else}}<input/>{{/}}';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
 });
@@ -256,75 +256,120 @@ describe('view', function() {
 
   it('<view name="tmp"></view>', function() {
     var html = '<view name="tmp"></view>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
 
   it('<view name="tmp"><br/></view>', function() {
     var html = '<view name="tmp"><br/></view>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('<view name="tmp"><br/><input/></view>', function() {
     var html = '<view name="tmp"><br/><input/></view>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('<view name="tmp" class="hello"></view>', function() {
     var html = '<view name="tmp" class="hello"></view>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('<view name="tmp" as="hello"></view>', function() {
     var html = '<view name="tmp" as="hello"></view>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('<view name="tmp" on-lick="hello()"></view>', function() {
     var html = '<view name="tmp" on-click="hello()"></view>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('Attributes - label: <view name="tmp"><label></label></view>', function() {
     var html = '<view name="tmp"><label></label></view>';
-    var template = getTemplate('tmp', html, {'attributes': 'label'});
-    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html, {'attributes': 'label'});
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('Attributes - label: <view name="tmp"><label><br/></label></view>', function() {
     var html = '<view name="tmp"><label><br/></label></view>';
-    var template = getTemplate('tmp', html, {'attributes': 'label'});
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html, {'attributes': 'label'});
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
   it('No Attributes - label: <view name="tmp"><label></label></view>', function() {
     var html = '<view name="tmp"><label></label></view>';
-    var template = getTemplate('tmp', html);
-//    console.log('serialization: ', template.serialize());
-    expect(template.deserialize()).equal(html);
+    var view = getView('tmp', html);
+//    console.log('serialization: ', view.template.serialize());
+    expect(view.deserialize()).equal(html);
   });
 
-//  it('Attributes - label: <view name="tmp"><label class="hello"></label></view>', function() {
-//    var html = '<view name="tmp"><label class="hello"></label></view>';
-//    var template = getTemplate('tmp', html, {'attributes': 'label'});
-//    console.log('serialization: ', template.serialize());
-//    expect(template.deserialize()).equal(html);
+//  it('Attributes - label: <view name="dropdown"><label class="mother"></label></view>', function() {
+//    var html = '<view name="dropdown"><label class="mother"></label></view>';
+//    var views= new templates.Views();
+//    view = views.register('dropdown', '{{@label.class}}', {'attributes': 'label'}
+//    );
+//    view.parse();
+//
+//    view = views.register('tmp', html);
+//    view.parse();
+//
+//    var ser = view.serialize();
+//    var des = view.template.deserialize();
+//
+//    console.log('serialization: ', ser);
+//
+//    expect(des).equal(html);
+//
 //  });
 
+  it('Arrays - option: <view name="dropdown"><option><br/><input/></option></view>', function() {
+    var html = '<view name="dropdown"><option><br/><input/></option></view>';
+    var views= new templates.Views();
+    view = views.register('dropdown', '{{@label.class}}', {'arrays': 'option/options'});
+    view.parse();
+
+    view = views.register('tmp', html);
+    view.parse();
+
+
+    var ser = view.serialize();
+//    console.log('serialization: ', ser);
+    var des = view.deserialize();
+    expect(des).equal(html);
+
+  });
+
+  it('Arrays - option: <view name="dropdown"><option class="hello"><br/></option></view>', function() {
+    var html = '<view name="dropdown"><option class="hello"><br/></option></view>';
+    var views= new templates.Views();
+    view = views.register('dropdown', '{{@label.class}}', {'arrays': 'option/options'});
+    view.parse();
+
+    view = views.register('tmp', html);
+    view.parse();
+
+
+    var ser = view.template.serialize();
+//    console.log('serialization: ', ser);
+    var des = view.deserialize();
+    expect(des).equal(html);
+
+  });
 
 //  {'attributes': 'label tip option-content', 'arrays': 'option/options', 'element': 'l-dropdown'}
 
@@ -346,12 +391,12 @@ describe('view', function() {
 
 
 
-function getTemplate(name, source, options){
+function getView(name, source, options){
   var views= new templates.Views();
   view = views.register(name, source, options);
   view.parse();
 
-  return view.template;
+  return view;
 }
 
 
